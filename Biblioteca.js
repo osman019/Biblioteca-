115a479ae33538fc230c75d00bd794c086ea5a08
osman019 <osmanortizrolon@gmail.com>
2024-08-28 20:43:55 -0500

    listadelibros

---
+++ Biblioteca.js
@@ -0,0 +1,20 @@
+const libros = [
+    {
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        paginas: 432,
+        prestado: false,
+    },
+    {
+        titulo: "1984",
+        autor: "George Orwell",
+        paginas: 328,
+        prestado: false,
+    },
+    {
+        titulo: "Don Quijote de la Mancha",
+        autor: "Miguel de Cervantes",
+        paginas: 863,
+        prestado: true,
+    },
+];
